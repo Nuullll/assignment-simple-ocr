cdbd8d3f9802277813418a2dc1bdae237f0a00ed
Yilong Guo <nuullll@Nuullll-MBP.local>
2018-04-02 21:13:00 +0800

    Remove test section.

--- src/run.m
+++ src/run.m
@@ -12,11 +12,4 @@ preprocess_train_dataset;
 %% preprocess test dataset
 preprocess_test_dataset;
 
-%% test section
-img = imread([TEST_DIR, '', '²¹³ä1.bmp']);
-W = wiener2(rgb2gray(img), [3 3]);
-bw = imbinarize(W, 'adaptive', 'ForegroundPolarity', 'dark', 'Sensitivity', 0.57);
-figure;
-imshowpair(img, W, 'montage');
-figure;
-imshowpair(W, bw, 'montage');
+%% 
